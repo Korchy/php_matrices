0ecaaa8b11a8701cb4d0bc6c77e2cb6feb900c11
Nikita Akimov <korchiy@yandex.ru>
2024-01-19 16:11:50 +0300

    update

--- php_matrices/Matrices3x3.inc.php
+++ php_matrices/Matrices3x3.inc.php
@@ -86,6 +86,15 @@ class Matrices3x3 {
 		];
 	}
 
+	public static function multiply_vector(array $matrix, array $vector): array {
+		// multiply $matrix by $vector[3]
+		return [
+			[$matrix[0][0] * $vector[0] + $matrix[0][1] * $vector[1] + $matrix[0][2] * $vector[2]],
+			[$matrix[1][0] * $vector[0] + $matrix[1][1] * $vector[1] + $matrix[1][2] * $vector[2]],
+			[$matrix[2][0] * $vector[0] + $matrix[2][1] * $vector[1] + $matrix[2][2] * $vector[2]]
+		];
+	}
+
 	public static function inverse(array $matrix) {
 		// get inverse matrix from $matrix
 		$det = static::det($matrix);
